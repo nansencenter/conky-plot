a232e0594b8cbfade178fda25e35a20d1177114e
Admin <sysadm@maud.ad.nersc.no>
2019-02-25 12:48:20 +0000

    updated plot

--- plot_cpu_usage.py
+++ plot_cpu_usage.py
@@ -12,27 +12,35 @@ parser.add_argument('-w', '--window', type=int, default=3, help='Sliding average
 parser.add_argument('-s', '--step', type=int, default=1, help='Plot each <s> record')
 args = parser.parse_args(sys.argv[1:])
 
-cpus = 8
-temps = 5
+cpus = 32
+temps = 4 
 
 plot_style = dict(legend = False)
 
-ifile = '/home/antonk/conky.txt'
+ifile = 'conky.txt'
 
 cpu_names = ['cpu%02d' % n for n in range(cpus)]
 freq_names = ['freq%02d' % n for n in range(cpus)]
 temp_names = ['temp%02d' % n for n in range(temps)]
 names = ['d0', 'd1']+cpu_names+freq_names+temp_names
 
-df = pd.read_csv(ifile, sep=" ", header=None, parse_dates={'time': [0,1]}, names=names).set_index('time')
-
-last_recs = df.index > (dt.datetime.now() - dt.timedelta(hours=args.hours))
-
 fig, axes = plt.subplots(nrows=3, ncols=1, sharex='col')
-ax0 = df[last_recs].rolling(args.window).mean()[::args.step].plot(y=cpu_names, ax=axes[0], **plot_style)
-ax0.set_ylabel('U, %')
-ax1 = df[last_recs].rolling(args.window).mean()[::args.step].plot(y=freq_names, ax=axes[1], **plot_style)
-ax1.set_ylabel('F, MHz')
-ax2 = df[last_recs].rolling(args.window).mean()[::args.step].plot(y=temp_names, ax=axes[2], **plot_style)
-ax2.set_ylabel('T, $^O$C')
+
+while True:
+    df = pd.read_csv(ifile, sep=" ", header=None, parse_dates={'time': [0,1]}, names=names).set_index('time')
+    last_recs = df.index > (dt.datetime.now() - dt.timedelta(hours=args.hours))
+    df = df[last_recs]
+
+    ax0 = df.rolling(args.window).mean()[::args.step].plot(y=cpu_names, ax=axes[0], **plot_style)
+    ax01 = df[cpu_names].mean(axis=1).plot(ax=axes[0], legend=False, ls='-', marker='.', color='k')
+    ax0.set_ylabel('U, %')
+    ax1 = df.rolling(args.window).mean()[::args.step].plot(y=freq_names, ax=axes[1], **plot_style)
+    ax11 = df[freq_names].mean(axis=1).plot(ax=axes[1], legend=False, ls='-', marker='.', color='k')
+    ax1.set_ylabel('F, MHz')
+    ax2 = df.rolling(args.window).mean()[::args.step].plot(y=temp_names, ax=axes[2], **plot_style)
+    ax21 = df[temp_names].mean(axis=1).plot(ax=axes[2], legend=False, ls='-', marker='.', color='k')
+    ax2.set_ylabel('T, $^O$C')
+
+    plt.pause(10)
+
 plt.show()
