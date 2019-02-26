7e9ac8e4298e3954cead9c785650d29e5c4e001f
akorosov <korosov@gmail.com>
2019-02-26 16:56:05 +0000

    better refresh

--- plot_cpu_usage.py
+++ plot_cpu_usage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 import argparse
 import datetime as dt
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 parser = argparse.ArgumentParser(description="Plot CPU usage, frequency and temperature")
-parser.add_argument('-n', '--hours', type=int, default=2, help='Show plots for the last <n> hours')
+parser.add_argument('-n', '--hours', type=float, default=2, help='Show plots for the last <n> hours')
 parser.add_argument('-w', '--window', type=int, default=3, help='Sliding average over <w> minutes')
 parser.add_argument('-s', '--step', type=int, default=1, help='Plot each <s> record')
 args = parser.parse_args(sys.argv[1:])
@@ -17,7 +18,8 @@ temps = 4
 
 plot_style = dict(legend = False)
 
-ifile = 'conky.txt'
+script_path = os.path.dirname(os.path.realpath(__file__))
+ifile = os.path.join(script_path, 'conky.txt')
 
 cpu_names = ['cpu%02d' % n for n in range(cpus)]
 freq_names = ['freq%02d' % n for n in range(cpus)]
@@ -26,7 +28,13 @@ names = ['d0', 'd1']+cpu_names+freq_names+temp_names
 
 fig, axes = plt.subplots(nrows=3, ncols=1, sharex='col')
 
+ax0 = None
+
 while True:
+    if not ax0 is None:
+        ax0.clear()
+        ax1.clear()
+        ax2.clear()
     df = pd.read_csv(ifile, sep=" ", header=None, parse_dates={'time': [0,1]}, names=names).set_index('time')
     last_recs = df.index > (dt.datetime.now() - dt.timedelta(hours=args.hours))
     df = df[last_recs]
@@ -40,7 +48,6 @@ while True:
     ax2 = df.rolling(args.window).mean()[::args.step].plot(y=temp_names, ax=axes[2], **plot_style)
     ax21 = df[temp_names].mean(axis=1).plot(ax=axes[2], legend=False, ls='-', marker='.', color='k')
     ax2.set_ylabel('T, $^O$C')
-
     plt.pause(10)
 
 plt.show()
